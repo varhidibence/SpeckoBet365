5bdb0d903967dc062d29c569b9bb14d6355d554d
Bence Várhidi <bence.varhidi@shareline.hu>
2022-12-15 16:46:08 +0100

    getActualNumberOfCorners init, jquery include problem

--- SpeckoBet365/scripts/content.js
+++ SpeckoBet365/scripts/content.js
@@ -1,8 +1,18 @@
 // starting webpage: https://www.bet365.com/#/IP/B1
 console.log("Starting SpeckoBet365 extension...")
+// var script = document.createElement('script');
+// script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+// document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(function () {
+    console.log("Searching actual number of corners...");
+    getActualNumberOfCorners();
+  });
+
 
 function navigateToLiveMatches() {
-    
+    var match = document.getElementsByClassName('ovm-FixtureDetailsTwoWay_TeamsWrapper');
+    console.log(match);
 }
 
 function navigateToFootballMatches() {
@@ -22,6 +32,24 @@ function getActualMatchTime() {
 }
 
 function getActualNumberOfCorners() {
-    
+    // var matchDetails = document.getElementsByClassName('ipe-EventViewDetail_ContentContainer');
+    // console.log(matchDetails[0]);
+    //var indexOfCorners = matchDetails.innerHtml.indexOf('Jelenlegi szögletek');
+    // if(indexOfCorners !== -1){ 
+    //     alert("searched string available"); 
+    //     console.log(document[indexOfCorners]);
+    //} 
+
+    var pattern = "Jelenlegi szögletek";
+  
+    var divs = document.getElementsByClassName("ipe-EventViewDetail_ContentContainer");
+    for (var i = 0; i < divs.length; i++) {
+       var para = divs[i];
+       var index = para.innerText.toLowerCase().indexOf(pattern);
+       if (index != -1) {
+            alert("searched string available");
+            break;
+       }
+    }  
 }
 
